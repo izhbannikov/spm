86d8973c68c4d0182382684b9d8bd8266f491182
izhbannikov <zhbannikov.ilya@gmail.com>
2015-06-23 17:47:48 +0300

    Minor bugs fixes.

--- R/simdata.R
+++ R/simdata.R
@@ -1,39 +1,33 @@
 simdata <-
-function(N=10, aH=-0.05, f1H=80, fH=80, bH=10, QH=1e-05, mu0H=2e-05, thetaH=0.08,m0=80, nj=10, step=0.05, trange=c(65,80), yrange = c(60,100), sd0=4) {
-  trapezoid <- function(fun, a, b, n=100,t) {
-    h <- (b-a)/n
-    x <- seq(a,b,by=h)
-    y <- fun(idx=x,t=t)
-    s <- h*(y[1]/2 + sum(y[2:n]) + y[n+1]/2)
-    s
-  }
+function(N=10, aH=-0.05, f1H=80, fH=80, bH=5, QH=2e-07, mu0H=2e-05, thetaH=0.08,m0=80, nj=10, step=0.05, trange=c(65,80), yrange = c(60,100), sd0=4) {
   
   Q <- function(t) {
-    Q <- QH*exp(thetaH*t)
+    Q <- QH#*exp(thetaH*t)
     Q
   }
   
-  mu0 <- function(t) {
-    mu0 <- mu0H*exp(thetaH*t)
-    mu0
+  mu <- function(t, par) {
+    hfH <- fH-par[1];
+    hf1H <- f1H-par[1];
+    
+    mu0Ht <- mu0H*exp(thetaH*t);
+    mu <- mu0Ht+hfH^2*QH+QH*par[2];
+    mu
   }
   
-  myfunc <- function(t, y, parms) {
-    dy1 <- -1*aH*(y[1] - f1H) + 2*y[2]*Q(t)*(y[1] - fH)
-    dy2 <- 2*aH*y[2] + bH^2 - 2*y[2]^2*Q(t)
-    list(c(dy1, dy2))
+  func1 <- function(t, y) {
+    hfH <- fH-y[1];
+    hf1H <- f1H-y[1];
+    #print(paste("hfH=",hfH,"hf1H=",hf1H))
+    dy1 <- -1.0*aH*hf1H + 2.0*y[2]*Q(t)*hfH
+    dy2 <- 2.0*aH*y[2] + bH - 2.0*y[2]^2*Q(t) #dy2 <- 2*aH*y[2] + bH^2 - 2*y[2]^2*Q(t)
+    c(dy1, dy2)
   }
-  
-  #mu <- function(t,idx) {
-  #  mu <- mu0(t) + Q(t)*(m[idx] - fH)^2 + Q(t)*gamma[idx]
-  #  mu
-  #}
   
   new_person <- T
   data <- matrix(nrow=1,ncol=5,0)
   record <- 1
   for(i in 1:N) {
-    #print(i)
     if(new_person == T) {
       t1 <- runif(1,trange[1], trange[2])
       #print(t1)
@@ -47,31 +41,63 @@ function(N=10, aH=-0.05, f1H=80, fH=80, bH=10, QH=1e-05, mu0H=2e-05, thetaH=0.08
         } 
       }
       
-      out <- euler(times = c(t1,t2), y = c(y1, 0), func = myfunc, parms = NULL)
-      m <- out[,2]
-      gamma <- out[,3]
-      #print(m)
-      #print(gamma)
+      nsteps <- 2.00
+      h=(t2-t1)/nsteps
       
-      mu <- c(0,0)
-      mu[1] <- mu0(t1) + Q(t1)*(m[1] - fH)^2 + Q(t1)*gamma[1]
-      mu[2] <- mu0(t2) + Q(t2)*(m[2] - fH)^2 + Q(t2)*gamma[2]
-      s <- (sum(mu)/2)*(t2-t1)
+      # Integration:
+      s <- h/3*(-1)*mu(t1,c(y1,0))
       
-      S <- exp(-1*s)
+      t <- t1
+      out <- c(y1,0)
+      for(j in 1:nsteps) {
+        # Runge-Kutta method:
+        k1ar <- func1(t,out)
+        yfin <- out + h/6.00*k1ar
+        ytmp <- out + h/2.00*k1ar
+        
+        k2ar <- func1(t,ytmp)
+        yfin <- yfin + h/3.00*k2ar
+        ytmp <- out + h/2.00*k2ar
+        
+        k3ar <- func1(t,ytmp)
+        yfin <- yfin + h/3.00*k3ar
+        ytmp <- out + h*k3ar
+        
+        k4ar <- func1(t,ytmp)
+        out <- yfin + h/6.00*k4ar
+        
+        t <- t + h
+        
+        # Integration:
+        if (j == nsteps) {
+          ifactor <- 1
+        } else {
+          if ((j %% 2) == 0) {
+            ifactor <- 2
+          } else {
+            ifactor <- 4
+          }
+        }
+        s <- s + ifactor*h/3.00*(-1)*mu(t,c(out[1],out[2]))
+        
+      }
+      
+      m <- out[1]
+      gamma <- out[2]
+      
+      S <- exp(s)
     
-      #S <- exp(-1*trapezoid(mu, 1,2,n=1,t2))
       #print(S)
       
       xi <- 0
-      if (S < runif(1,0.95,1)) {
+      if (S < runif(1,0.996,1)) {
         xi <- 0
       } else {
         xi <- 1
       }
       
       if(xi == 0) {
-        y2 <- rnorm(1,mean=m[2], sd=sqrt(gamma))
+        y2 <- rnorm(1,mean=m, sd=sqrt(gamma))
         new_person <- F
       } else {
         y2 <- NA
@@ -82,34 +108,70 @@ function(N=10, aH=-0.05, f1H=80, fH=80, bH=10, QH=1e-05, mu0H=2e-05, thetaH=0.08
       record <- record + 1
     } 
     
-    j <- 1
+    jj <- 1
     while(new_person == F){
       t1 <- t2
       t2 <- t1 + 2*runif(1,0,1) + step
       y1 <- y2
       
-      out <- euler(times = c(t1,t2), y = c(y1, 0), func = myfunc, parms = NULL)
-      m <- out[,2]
-      gamma <- out[,3]
+      nsteps <- 2.00
+      h=(t2-t1)/nsteps
       
-      mu <- c(0,0)
-      mu[1] <- mu0(t1) + Q(t1)*(m[1] - fH)^2 + Q(t1)*gamma[1]
-      mu[2] <- mu0(t2) + Q(t2)*(m[2] - fH)^2 + Q(t2)*gamma[2]
-      s <- (sum(mu)/2)*(t2-t1)
+      # Integration:
+      s <- h/3*(-1)*mu(t1,c(y1,0))
       
-      S <- exp(-1*s)
+      t <- t1
+      out <- c(y1,0)
+      for(j in 1:nsteps) {
+        # Runge-Kutta method:
+        k1ar <- func1(t,out)
+        yfin <- out + h/6.00*k1ar
+        ytmp <- out + h/2.00*k1ar
+        
+        k2ar <- func1(t,ytmp)
+        yfin <- yfin + h/3.00*k2ar
+        ytmp <- out + h/2.00*k2ar
+        
+        k3ar <- func1(t,ytmp)
+        yfin <- yfin + h/3.00*k3ar
+        ytmp <- out + h*k3ar
+        
+        k4ar <- func1(t,ytmp)
+        out <- yfin + h/6.00*k4ar
+        
+        t <- t + h
+        
+        # Integration:
+        if (j == nsteps) {
+          ifactor <- 1
+        } else {
+          if ((j %% 2) == 0) {
+            ifactor <- 2
+          } else {
+            ifactor <- 4
+          }
+        }
+        s <- s + ifactor*h/3.00*(-1)*mu(t,c(out[1],out[2]))
+        
+        
+      }
       
-      #S <- exp(-1*trapezoid(mu, 1,2,n=1,t2))
+      m <- out[1]
+      gamma <- out[2]
+      
+      S <- exp(s)
+      
       #print(S)
+      
       xi <- 0
-      if (S < runif(1,0,1)) {
+      if (S < runif(1,0.996,1)) {
         xi <- 0
       } else {
         xi <- 1
       }
       
       if(xi == 0) {
-        y2 <- rnorm(1,mean=m[2], sd=sqrt(gamma))
+        y2 <- rnorm(1,mean=m, sd=sqrt(gamma))
         new_person <- F
       } else {
         y2 <- NA
@@ -121,8 +183,8 @@ function(N=10, aH=-0.05, f1H=80, fH=80, bH=10, QH=1e-05, mu0H=2e-05, thetaH=0.08
         record <- record + 1
       }
       
-      j <- j + 1
-      if(j == nj) {
+      jj <- jj + 1
+      if(jj == nj) {
         new_person <- T
       }
     }
