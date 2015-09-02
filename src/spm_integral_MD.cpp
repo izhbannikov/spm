c65df893dc6272d15ca0e729630537377050c171
izhbannikov <zhbannikov.ilya@gmail.com>
2015-09-02 17:27:48 +0300

    Bugs fixes

--- src/spm_integral_MD.cpp
+++ src/spm_integral_MD.cpp
@@ -96,7 +96,7 @@ RcppExport SEXP complikMD(SEXP dat, SEXP n, SEXP m, SEXP ah, SEXP f1h, SEXP qh, 
     arma::mat gamma1(dim,dim);
     
     L = 0;
-    double  nsteps = 2;
+    double  nsteps = 10;
       
     for(int i=0; i<N; i++) {
       //Solving differential equations on intervals:
@@ -112,7 +112,7 @@ RcppExport SEXP complikMD(SEXP dat, SEXP n, SEXP m, SEXP ah, SEXP f1h, SEXP qh, 
       
       double tdiff = t2-t1;
       if(tdiff > 2) {
-        nsteps = 2*tdiff;
+        nsteps = 10*tdiff;
       }
       
       double h = tdiff/nsteps;
