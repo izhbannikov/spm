42607d52522ba3264360fa99a956f6834b7274ad
izhbannikov <zhbannikov.ilya@gmail.com>
2015-09-03 23:47:20 +0300

    Bugs fixes

--- R/spm_integral_MD.R
+++ R/spm_integral_MD.R
@@ -106,8 +106,8 @@ spm_integral_MD <- function(dat,parameters) {
       dims <- dim(dat)
       res <- .Call("complikMD", dat, dims[1], dims[2], a, f1, Q, b, f, mu0, theta, kk)
     } else {
-      final_res <<- list(results)
-      stop("Optimization stopped. Parametes achieved lower or upper bound, you need more data to correctrly obtain optimal parameters.")
+      cat("Optimization stopped. Parametes achieved lower or upper bound, you need more data to correctrly obtain optimal parameters.")
+      return(final_res <<- list(results))    
     }
     
     iteration <<- iteration + 1
