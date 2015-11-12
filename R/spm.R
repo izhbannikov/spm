e46ddf5d236fd81d613ddf208b986d2e91017745
izhbannikov <zhbannikov.ilya@gmail.com>
2015-11-12 03:54:55 +0300

    Docs updated.

--- R/spm.R
+++ R/spm.R
@@ -9,17 +9,17 @@
 #'(2) Estimated coefficients.
 #'@examples
 #'library(spm)
-#'# Reading longitude data:
+#'# Reading data
+#'## Longitudinal studies:
 #'longdat <- read.csv(system.file("data","longdat.csv",package="spm"))
-#'# Prepare data for optimization:
+#'## Vital statistics:
 #'vitstat <- read.csv(system.file("data","vitstat.csv",package="spm"))
-#'# Remove unneeded NAs:
-#'longdat.nonan <- longdat[which(is.na(longdat$Age) == F),]
-#'vitstat.nonan <- vitstat[which(is.na(vitstat$BirthCohort) == F),]
-#'data=prepare_data(longdat=longdat.nonan, vitstat=vitstat.nonan,interval=1, col.status="IsDead", col.id="ID", col.age="Age", col.age.next="AgeNext", col.age.event="LSmort", covariates=c("DBP"), verbose=T)
+#'# Prepare data for optimization:
+#'data=prepare_data(longdat=longdat, vitstat=vitstat,interval=1, col.status="IsDead", col.id="ID", col.age="Age", col.age.event="LSmort", covariates=c("DBP"), verbose=T)
 #'# Parameters estimation:
 #'pars=spm(data,k = 1)
 #'pars
+#'
 spm <- function(dat,k=2, verbose=F, tol=NULL) {
   # Main function for Stochastic Process Modelling package
   # Parameters: 
