9b2d3ee8663b7f4b54ff0bd60d7dffffd35caa41
izhbannikov <zhbannikov.ilya@gmail.com>
2015-12-22 19:59:33 +0300

    Many changes, funcitons spm() and spm_time_dep() updated.

--- R/spm.R
+++ R/spm.R
@@ -1,12 +1,15 @@
 #'Stochastic Process Modelling (SPM)
 #'A main function that estimates parameters a, f1, Q, f, b, mu0, theta
 #'from given dataset.
-#'@param dat A dataset.
-#'@param k Number of dimensions.
+#'@param x A dataset.
+#'@param model A model type: c("continuous", "time-dependent").
+#'@param formulas A list of formulas used in time-dependent model.
 #'@param verbose A verbosing output indicator.
 #'@param tol A tolerance threshold for matrix inversion.
-#'@return A list of (1) Estimated starting point (from quick discrete optimization) and 
-#'(2) Estimated coefficients.
+#'@return A list of (1) coefficient estimates from discrete model and 
+#'(if appropriate model type was provided): 
+#'(2) coefficient estimates from continuous model and
+#'(3) coefficient estimates from time-dependent model.
 #' @examples
 #'library(spm)
 #'#Prepare data for optimization
@@ -14,38 +17,57 @@
 #'vitstat <- read.csv(system.file("data","vitstat.csv",package="spm"))
 #' data=prepare_data(longdat=longdat, vitstat=vitstat,interval=1, col.status="IsDead", col.id="ID", col.age="Age", col.age.event="LSmort", covariates=c("DBP"), verbose=T)
 #'#Parameters estimation:
-#'pars=spm(data,k = 1)
+#'pars=spm(data)
 #'pars
-spm <- function(dat,k=2, verbose=F, tol=NULL) {
+spm <- function(x, model=NULL, formulas = NULL, verbose=F, tol=NULL) {
   # Main function for Stochastic Process Modelling package
   # Parameters: 
   # - dat - input data frame
-  # - k - number of dimensions
   # - tol - tolerance threshold for matrix inversion
-  #
-  # Step 1: estimation of starting point with quick discrete optimization:
-  pars=spm_discrete(dat=dat[[2]],k=k)
+  
+  # List of available models:
+  models <- c("continuous", "time-dependent")
+  
+  if(!is.null(model)) {
+    if(length(unique(model)) > 2) {
+      stop("Only continuous and/or time-dependent models can be used.")
+    }
+    for(i in 1:length(model)) {
+      if(!(model[i] %in% models)) {
+        cat("Warning: ", model[i], " - unknown model type!")
+      }
+    }
+  }
+  
+  # Number of variables (dimensions):
+  k <- (dim(x[[1]])[2] - 4)/2
+  
+  # Estimation of starting point with discrete optimization:
+  pars=spm_discrete(dat=x[[2]],k=k)
   
   if(verbose) {
     cat("Starting parameters:\n")
     print(pars)
   }
   
-  # Step2: continuous slow optimization:
-  data <- dat[[1]][,2:dim(dat[[1]])[2]]
+  res <- list(estimated.discrete=list(Q=pars$pars2$Q, a=pars$pars2$a, b=pars$pars2$b, f1=pars$pars2$f1, f=pars$pars2$f, mu0=pars$pars2$mu0, theta=pars$pars2$theta))
   
-  if(det(pars$pars2$Q) < 0) {
-    cat("Error: determinant of Q < 0\n")
-    cat("Q:\n")
-    print(pars$pars2$Q)
-    cat("Det(Q):\n")
-    print(det(pars$pars2$Q))
-    res=list(starting=list(Q=pars$pars2$Q, a=pars$pars2$a, b=pars$pars2$b, f1=pars$pars2$f1, f=pars$pars2$f, mu0=pars$pars2$mu0, theta=pars$pars2$theta), 
-             estimated=NA)
+  #If required, continuous-time optimization:
+  if("continuous" %in% model) {
+    data <- x[[1]][,2:dim(x[[1]])[2]]
+  
+    if(det(pars$pars2$Q) < 0) {
+      cat("Error: determinant of Q < 0\n")
+      cat("Q:\n")
+      print(pars$pars2$Q)
+      cat("Det(Q):\n")
+      print(det(pars$pars2$Q))
+      
+      res[["estimated.continuous"]] <- NA
     
-  } else {
+    } else {
   
-    spm_continuous(data, 
+      spm_continuous(data, 
                     a=pars$pars2$a, 
                     f1=pars$pars2$f1, 
                     Q=pars$pars2$Q, 
@@ -56,8 +78,38 @@ spm <- function(dat,k=2, verbose=F, tol=NULL) {
                     k, 
                     verbose)
  
-    res=list(starting=list(Q=pars$pars2$Q, a=pars$pars2$a, b=pars$pars2$b, f1=pars$pars2$f1, f=pars$pars2$f, mu0=pars$pars2$mu0, theta=pars$pars2$theta), 
-           estimated=get("results",envir=.GlobalEnv))
+      res[["estimated.continuous"]] <- get("results",envir=.GlobalEnv)
+    }
   }
+  
+  if("time-dependent" %in% model) {
+    data <- x[[1]][,2:dim(x[[1]])[2]]
+    if(k > 1) {
+      stop("Number of variables > 1. Model with time-dependent parameters can be used only with one variable!")
+    }
+    
+    spm_time_dep <- function(data, 
+                             start=list(a1=0, a2=estimated.discrete$a, f1=80, Q=2e-8, f=80, b=5, mu0=1e-5, theta=0.08),
+                             formulas=list(at="a1*t+a2", f1t="f1", Qt="Q*exp(theta*t)", ft="f", bt="b", mu0t="mu0*exp(theta*t)"), 
+                             verbose=TRUE,
+                             lower_bound=NULL, upper_bound=NULL, factr=1e-16, lmult=0.5, umult=2) {
+      
+    
+    spm_continuous(data, 
+                     a=pars$pars2$a, 
+                     f1=pars$pars2$f1, 
+                     Q=pars$pars2$Q, 
+                     f=pars$pars2$f, 
+                     b=pars$pars2$b, 
+                     mu0=pars$pars2$mu0, 
+                     theta=pars$pars2$theta, 
+                     k, 
+                     verbose)
+      
+      res=list(starting=list(Q=pars$pars2$Q, a=pars$pars2$a, b=pars$pars2$b, f1=pars$pars2$f1, f=pars$pars2$f, mu0=pars$pars2$mu0, theta=pars$pars2$theta), 
+               estimated=get("results",envir=.GlobalEnv))
+    
+  }
+  
   invisible(res)
 }
--- R/spm_time-dependent.R
+++ R/spm_time-dependent.R
@@ -7,7 +7,16 @@ trim.trailing <- function (x) sub("\\s+$", "", x)
 # returns string w/o leading or trailing whitespace
 trim <- function (x) gsub("^\\s+|\\s+$", "", x)
 
-optimize <- function(data, starting_params,  formulas, verbose, lower_bound, upper_bound, factr=factr) {
+optimize <- function(data, starting_params,  formulas, verbose, 
+                     lower_bound, upper_bound, 
+                     factr=1e-16, lmult=0.5,umult=2) {
+  ### for test purposes:
+  #data <- data1[[1]][,2:6]
+  #starting_params <- list(a=-0.5, f1=80, Q=2e-8, f=80, b=5, mu0=1e-5)
+  #formulas <- list(at="a1*t+a2", f1t="f1+f33*t", Qt="Q*exp(theta*t)", ft="f*t+ff", bt="b", mu0t="mu0*exp(theta*t)")
+  #verbose=TRUE
+  ### End of test purposes
+  
   final_res <- list()
   # Current results:
   results <- list()
@@ -22,44 +31,193 @@ optimize <- function(data, starting_params,  formulas, verbose, lower_bound, upp
   variables <- c()
   
   # Assigning parameters:
+  p.const.ind <- c()
+  p.coeff.ind <- c()
+  
+  
   #---
   parameters <- trim(unlist(strsplit(formulas$at,"[\\+\\*\\(\\)]",fixed=F)))
   parameters <- parameters[which(!(parameters %in% c("t","exp")))]
   for(p in parameters) {assign(p,NULL, envir = .GlobalEnv); variables <- c(variables, p);}
+  variables <- unique(variables)
+  p.constants <- c()
+  p.coeffs <- c()
+  p.temp <- trim(unlist(strsplit(formulas$at,"[\\+\\-\\(\\)]",fixed=F)))
+  for(i in 1:length(p.temp)) {
+    if(grepl('t', p.temp[i]) == FALSE) {
+      p.constants <- c(p.constants, p.temp[i])
+    } else {
+      p.temp.coeff <- trim(unlist(strsplit(p.temp[i],"[\\*]",fixed=F)))
+      p.coeffs <- c(p.coeffs, p.temp.coeff[which(!(p.temp.coeff %in% c("t","exp")))])
+    }
+  }
+  
+  for(i in 1:length(p.constants)) {
+    p.const.ind <- c(p.const.ind, which(variables == p.constants[i]))
+  }
+  for(i in 1:length(p.coeffs)) {
+    p.coeff.ind <- c(p.coeff.ind, which(variables == p.coeffs[i]))
+  }
   
   #----
   parameters <- trim(unlist(strsplit(formulas$f1t,"[\\+\\*\\(\\)]",fixed=F)))
   parameters <- parameters[which(!(parameters %in% c("t","exp")))]
-  for(p in parameters) {assign(p,NULL, envir = .GlobalEnv); variables <- c(variables, p);}  
+  for(p in parameters) {assign(p,NULL, envir = .GlobalEnv); variables <- c(variables, p);} 
+  variables <- unique(variables)
+  p.constants <- c()
+  p.coeffs <- c()
+  p.temp <- trim(unlist(strsplit(formulas$f1t,"[\\+\\-\\(\\)]",fixed=F)))
+  for(i in 1:length(p.temp)) {
+    if(grepl('t', p.temp[i]) == FALSE) {
+      p.constants <- c(p.constants, p.temp[i])
+    } else {
+      p.temp.coeff <- trim(unlist(strsplit(p.temp[i],"[\\*]",fixed=F)))
+      p.coeffs <- c(p.coeffs, p.temp.coeff[which(!(p.temp.coeff %in% c("t","exp")))])
+    }
+  }
+  
+  for(i in 1:length(p.constants)) {
+    p.const.ind <- c(p.const.ind, which(variables == p.constants[i]))
+  }
+  for(i in 1:length(p.coeffs)) {
+    p.coeff.ind <- c(p.coeff.ind, which(variables == p.coeffs[i]))
+  }
   
   #---
   parameters <- trim(unlist(strsplit(formulas$Qt,"[\\+\\*\\(\\)]",fixed=F)))
   parameters <- parameters[which(!(parameters %in% c("t","exp")))]
   for(p in parameters) {assign(p,NULL, envir = .GlobalEnv); variables <- c(variables, p);}
+  variables <- unique(variables)
+  p.constants <- c()
+  p.coeffs <- c()
+  p.temp <- trim(unlist(strsplit(formulas$Qt,"[\\+\\-\\(\\)]",fixed=FALSE)))
+  for(i in 1:length(p.temp)) {
+    if(grepl("*t", p.temp[i]) == FALSE) {
+        p.temp.coeff <- trim(unlist(strsplit(p.temp[i],"[\\*]",fixed=FALSE)))
+        p.constants <- c(p.constants,p.temp.coeff[which(!(p.temp.coeff %in% c("t","exp")))])
+    } else {
+        p.temp.coeff <- trim(unlist(strsplit(p.temp[i],"[\\*]",fixed=FALSE)))
+        p.coeffs <- c(p.coeffs, p.temp.coeff[which(!(p.temp.coeff %in% c("t","exp")))])
+    }
+  }
+  
+  for(i in 1:length(p.constants)) {
+    p.const.ind <- c(p.const.ind, which(variables == p.constants[i]))
+  }
+  for(i in 1:length(p.coeffs)) {
+    p.coeff.ind <- c(p.coeff.ind, which(variables == p.coeffs[i]))
+  }
   
   #---
   parameters <- trim(unlist(strsplit(formulas$ft,"[\\+\\*\\(\\)]",fixed=F)))
   parameters <- parameters[which(!(parameters %in% c("t","exp")))]
   for(p in parameters) {assign(p,NULL, envir = .GlobalEnv); variables <- c(variables, p);}
+  variables <- unique(variables)
+  p.constants <- c()
+  p.coeffs <- c()
+  p.temp <- trim(unlist(strsplit(formulas$ft,"[\\+\\-\\(\\)]",fixed=F)))
+  for(i in 1:length(p.temp)) {
+    if(grepl('t', p.temp[i]) == FALSE) {
+      p.constants <- c(p.constants, p.temp[i])
+    } else {
+      p.temp.coeff <- trim(unlist(strsplit(p.temp[i],"[\\*]",fixed=F)))
+      p.coeffs <- c(p.coeffs, p.temp.coeff[which(!(p.temp.coeff %in% c("t","exp")))])
+    }
+  }
+  
+  for(i in 1:length(p.constants)) {
+    p.const.ind <- c(p.const.ind, which(variables == p.constants[i]))
+  }
+  for(i in 1:length(p.coeffs)) {
+    p.coeff.ind <- c(p.coeff.ind, which(variables == p.coeffs[i]))
+  }
   
   #---
   parameters <- trim(unlist(strsplit(formulas$bt,"[\\+\\*\\(\\)]",fixed=F)))
   parameters <- parameters[which(!(parameters %in% c("t","exp")))]
   for(p in parameters) {assign(p,NULL, envir = .GlobalEnv); variables <- c(variables, p);}
+  variables <- unique(variables)
+  p.constants <- c()
+  p.coeffs <- c()
+  p.temp <- trim(unlist(strsplit(formulas$bt,"[\\+\\-\\(\\)]",fixed=F)))
+  for(i in 1:length(p.temp)) {
+    if(grepl('t', p.temp[i]) == FALSE) {
+      p.constants <- c(p.constants, p.temp[i])
+    } else {
+      p.temp.coeff <- trim(unlist(strsplit(p.temp[i],"[\\*]",fixed=F)))
+      p.coeffs <- c(p.coeffs, p.temp.coeff[which(!(p.temp.coeff %in% c("t","exp")))])
+    }
+  }
+  
+  for(i in 1:length(p.constants)) {
+    p.const.ind <- c(p.const.ind, which(variables == p.constants[i]))
+  }
+  for(i in 1:length(p.coeffs)) {
+    p.coeff.ind <- c(p.coeff.ind, which(variables == p.coeffs[i]))
+  }
   
   #---
   parameters <- trim(unlist(strsplit(formulas$mu0t,"[\\+\\*\\(\\)]",fixed=F)))
   parameters <- parameters[which(!(parameters %in% c("t","exp")))]
   for(p in parameters) {assign(p,NULL, envir = .GlobalEnv); variables <- c(variables, p);}
-  
-  
   variables <- unique(variables)
-  
-  for(p in names(starting_params)) {
-    results[[p]] <- starting_params[[p]]
-    assign(p, starting_params[[p]], envir = globalenv())
+  p.constants <- c()
+  p.coeffs <- c()
+  p.temp <- trim(unlist(strsplit(formulas$mu0t,"[\\+\\-\\(\\)]",fixed=F)))
+  for(i in 1:length(p.temp)) {
+    if(grepl("*t", p.temp[i]) == FALSE) {
+      p.temp.coeff <- trim(unlist(strsplit(p.temp[i],"[\\*]",fixed=FALSE)))
+      p.constants <- c(p.constants,p.temp.coeff[which(!(p.temp.coeff %in% c("t","exp")))])
+    } else {
+      p.temp.coeff <- trim(unlist(strsplit(p.temp[i],"[\\*]",fixed=FALSE)))
+      p.coeffs <- c(p.coeffs, p.temp.coeff[which(!(p.temp.coeff %in% c("t","exp")))])
+    }
   }
   
+  for(i in 1:length(p.constants)) {
+    p.const.ind <- c(p.const.ind, which(variables == p.constants[i]))
+  }
+  for(i in 1:length(p.coeffs)) {
+    p.coeff.ind <- c(p.coeff.ind, which(variables == p.coeffs[i]))
+  }
+  
+  ########=============
+  p.const.ind <- unique(p.const.ind)
+  p.coeff.ind <- unique(p.coeff.ind)
+  
+  #variables <- unique(variables)
+  stpar <- rep(0, length(variables))
+  for(i in 1:length(stpar)) {
+    stpar[p.const.ind[i]] <- unlist(starting_params, use.names = FALSE)[i]
+  }
+  names(stpar) <- variables
+  #stpar <- list(stpar)
+  
+  for(p in names(stpar)) {
+    results[[p]] <- stpar[p]
+    assign(p, stpar[p], envir = globalenv())
+  }
+  
+  #print(stpar)
+  
+  
+  # Lower and upper boundaries calculation:
+  #if(is.null(lower_bound)) {
+    lower_bound <- c()
+    for(i in 1:length(stpar)) {
+      if(stpar[[i]] == 0) {stpar[[i]] = 1e-5}
+      lower_bound <- c(lower_bound, ifelse(stpar[[i]] < 0, stpar[[i]] + lmult*stpar[[i]], stpar[[i]] - lmult*stpar[[i]]))
+    }
+  #}
+  
+  #if(is.null(upper_bound)) {
+    upper_bound <- c()
+    for(i in 1:length(stpar)) {
+      if(stpar[[i]] == 0) {stpar[[i]] = 1e-5}
+      upper_bound <- c(upper_bound, ifelse(stpar[[i]] < 0, stpar[[i]] - umult*stpar[[i]], stpar[[i]] + umult*stpar[[i]]))
+    }
+  #}
+  print(lower_bound)
   
   comp_func_params <- function(astring, f1string, qstring, fstring, bstring, mu0string) {
     at <<- eval(bquote(function(t) .(parse(text = astring)[[1]])))
@@ -79,7 +237,7 @@ optimize <- function(data, starting_params,  formulas, verbose, lower_bound, upp
     if(verbose)
       cat("Iteration: ", iteration, "\n")
     
-    for(p in names(starting_params)) {
+    for(p in names(stpar)) {
       assign(p, params[[p]], envir = globalenv())
       results[[p]] <<- params[[p]]
       if(verbose)
@@ -103,14 +261,14 @@ optimize <- function(data, starting_params,  formulas, verbose, lower_bound, upp
       ans <- mu0t(t) + (y - ft(t))^2*Qt(t)
     }
     
-    for(i in 1:length(results)) {
-      if(length(intersect(results[[i]],c(lower_bound[i], upper_bound[i]))) >= 2) {
-        cat("Parameter", names(results)[i], "achieved lower/upper bound. Process stopped.\n")
-        cat(results[[i]],"\n")
-        stopflag <- TRUE
-        break
-      }
-    }
+    #for(i in 1:length(results)) {
+    #  if(length(intersect(results[[i]],c(lower_bound[i], upper_bound[i]))) >= 2) {
+    #    cat("Parameter", names(results)[i], "achieved lower/upper bound. Process stopped.\n")
+    #    cat(results[[i]],"\n")
+    #    stopflag <- TRUE
+    #    break
+    #  }
+    #}
   
     L <- 0
     
@@ -154,13 +312,14 @@ optimize <- function(data, starting_params,  formulas, verbose, lower_bound, upp
   
   # Check if function parameters are consistent to those provided in starting list:
   if(verbose) {
+    print(names(stpar))
     print(names(starting_params))
     print(variables)
   }
   
-  if( setequal(names(starting_params), variables) == FALSE) {
-    stop("Provided set of function parameters is not equal to that one provided in starting list or vise-versa.")
-  }
+  #if( setequal(names(starting_params), variables) == FALSE) {
+  #  stop("Provided set of function parameters is not equal to that one provided in starting list or vise-versa.")
+  #}
   
   if(verbose == TRUE) {
     cat("Variables:\n")
@@ -175,18 +334,10 @@ optimize <- function(data, starting_params,  formulas, verbose, lower_bound, upp
   }
   
   ## Optimization:
-  #result <- optim(par = unlist(starting_params), 
-  #                                fn=maxlik_t, dat = as.matrix(data), 
-  #                               control = list(fnscale=-1, trace=T, maxit=10000, ndeps=replicate(factr,n=length(lower_bound))), 
-  #                                method="L-BFGS-B", 
-  #                                lower=lower_bound,
-  #                                upper=upper_bound)
-  
-  
-  tryCatch(optim(par = unlist(starting_params), 
+  tryCatch(optim(par = unlist(stpar), 
                   fn=maxlik_t, dat = as.matrix(data), 
-                 control = list(fnscale=-1, trace=TRUE, maxit=10000, ndeps=replicate(factr,n=length(lower_bound))), 
-                  method="L-BFGS-B", 
+                 control = list(fnscale=-1, trace=TRUE, maxit=10000), #ndeps=replicate(factr,n=length(lower_bound))), 
+                  method="L-BFGS-B",#, 
                   lower=lower_bound,
                   upper=upper_bound),
            error=function(e) {print(e)}, 
@@ -197,6 +348,7 @@ optimize <- function(data, starting_params,  formulas, verbose, lower_bound, upp
 }
 
 #' spm_time_dep : a function that can handle time-dependant coefficients:
+#' @param x : input data table.
 #' @param start : a list of starting parameters, default: llist(a=-0.5, f1=80, Q=2e-8, f=80, b=5, mu0=1e-5, theta=0.08),
 #' @param formulas : a list of formulas that define age (time) - dependency. Default: list(at="a1*t+a2", f1t="f1", Qt="Q*exp(theta*t)", ft="f", bt="b", mu0t="mu0*exp(theta*t)")
 #' @return optimal coefficients
@@ -207,33 +359,17 @@ optimize <- function(data, starting_params,  formulas, verbose, lower_bound, upp
 #' data <- simdata_time_dep(N=2500)
 #' opt.par <- spm_time_dep(data[,2:6], formulas=list(at="a", f1t="f1", Qt="Q*exp(theta*t)", ft="f", bt="b", mu0t="mu0*exp(theta*t)"), start=list(a=-0.5, f1=80, Q=2e-8, f=80, b=5, mu0=1e-5, theta=0.08))
 #' opt.par
-spm_time_dep <- function(data, 
-                         start=list(a1=-0.5, a2=0.2, f1=80, Q=2e-8, f=80, b=5, mu0=1e-5, theta=0.08),
+spm_time_dep <- function(x, 
+                         start=list(a1=-0.5, a2=0.2, f1=80, Q=2e-8, f=80, b=5, mu0=1e-5),
                          formulas=list(at="a1*t+a2", f1t="f1", Qt="Q*exp(theta*t)", ft="f", bt="b", mu0t="mu0*exp(theta*t)"), 
                          verbose=TRUE,
-                         lower_bound=NULL, upper_bound=NULL, factr=1e-16, lmult=0.5, umult=2) {
+                         lower_bound=NULL, upper_bound=NULL, 
+                         factr=1e-16, lmult=0.5, umult=2) {
   
-  # Values for lower/upper boundaries could be: 
-  #lower_bound=c(-1, 0, 2e-9, 0, 0, 0, 0, 0), upper_bound=c(-0.001, Inf, 1e-5, 1e-5, Inf, Inf, 1e-3, Inf)
-  
-  # Lower and upper boundaries calculation:
-  if(is.null(lower_bound)) {
-    lower_bound <- c()
-    for(i in 1:length(start)) {
-      if(start[[i]] == 0) {start[[i]] = 1e-5}
-      lower_bound <- c(lower_bound, ifelse(start[[i]] < 0, start[[i]] + lmult*start[[i]], start[[i]] - lmult*start[[i]]))
-    }
-  }
-  
-  if(is.null(upper_bound)) {
-    upper_bound <- c()
-    for(i in 1:length(start)) {
-      if(start[[i]] == 0) {start[[i]] = 1e-5}
-      upper_bound <- c(upper_bound, ifelse(start[[i]] < 0, start[[i]] - umult*start[[i]], start[[i]] + umult*start[[i]]))
-    }
-  }
+  data <- x
   
   # Optimization:
-  res = optimize(data, start, formulas, verbose, lower_bound, upper_bound, factr)
+  #res = optimize(data, start, formulas, verbose, lower_bound, upper_bound, factr)
+  res = optimize(data, start, formulas, verbose, factr=1e-16, lmult=lmult, umult=umult)
   invisible(res)
 }
