242de0b7aaddcb5a37f3b72fee21172a88a9d63e
izhbannikov <zhbannikov.ilya@gmail.com>
2016-09-25 18:44:06 -0400

    Minor bug fixes.

--- R/spm_discrete.R
+++ R/spm_discrete.R
@@ -70,7 +70,7 @@ spm_discrete <- function(dat, theta_range=seq(0.02,0.2,by=0.001), tol=NULL, verb
                     control=list(maxit = 250, trace=verbose))
     
     if(verbose) {cat(coef(res.pois))}
-    
+    res <- res.pois
     tryCatch({res <- glm(reg_formula, data=as.data.frame(newdat), family = binomial(link = log), 
                          start=coef(res.pois), 
                          control=list(maxit = 250, trace=verbose))
@@ -100,10 +100,12 @@ spm_discrete <- function(dat, theta_range=seq(0.02,0.2,by=0.001), tol=NULL, verb
       index_j <- index_j  + 1
     }
     colnames(newdat2) <- cnames
-  
+    #print(head(newdat2))
     reg_formula <- paste(cnames[1],"~", paste(cnames[2:length(cnames)],collapse='+'))
     
-    res <- lm(as.formula(reg_formula),data=as.data.frame(newdat2))
+    res <- lm(as.formula(reg_formula),data=as.data.frame(newdat2),na.action = na.omit)
+    #res <- gls(as.formula(reg_formula),data=as.data.frame(newdat2),na.action = na.omit)
+    #print(reg_formula)
     coef <- res$coefficients # intercept, y1
     parameters_lsq <- rbind(parameters_lsq, c(coef, sd(res$residuals), logLik(res)[1]))
   
