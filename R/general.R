17d3b393c884ddeac38198fe1bd1de3988404330
izhbannikov <zhbannikov.ilya@gmail.com>
2017-02-10 22:05:44 -0500

    Minor bug with mice fixed

--- R/general.R
+++ R/general.R
@@ -1,10 +1,11 @@
 #' @import Rcpp
-#' @import mice
 #' @import sas7bdat
 #' @import stats
 #' @import nloptr
 #' @import survival
 #' @import tools
 #' @importFrom utils head read.csv tail
+#' @importFrom mice mice complete
+#' @importFrom mice mice.impute.pmm
 #' @useDynLib stpm
 NULL
