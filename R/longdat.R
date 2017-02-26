d110fb161ee89631de7921512f4ff491ee89943e
izhbannikov <zhbannikov.ilya@gmail.com>
2017-02-26 12:48:46 -0500

    Minor code refractoring

--- R/longdat.R
+++ R/longdat.R
@@ -4,3 +4,10 @@
 #' @author Ilya Y Zhbannikov \email{ilya.zhbannikov@duke.edu}
 #' @keywords data
 NULL
+
+#' This is the longitudinal genetic dataset.
+#' @name ex_data
+#' @docType data
+#' @author Liang He
+#' @keywords data
+NULL
--- R/spm_con_1d_g.r
+++ R/spm_con_1d_g.r
@@ -41,10 +41,13 @@
 #' @references He, L., Zhbannikov, I., Arbeev, K. G., Yashin, A. I., and Kulminski, A.M., 2017. 
 #' Genetic stochastic process model for detecting pleiotropic and interaction effects with longitudinal data.
 #' @export
-#' @examples { 
+#' @examples \dontrun{ 
 #' library(stpm) 
-#' load('ex_spmcon1dg.rda')
-#' res <- spm_con_1d_g(ex_data$spm_data, ex_data$gene_data, a = -0.02, b=0.2, q=0.01, f=3, f1=3, mu0=0.01, theta=1e-05, upper=c(-0.01,3,0.1,10,10,0.1,1e-05), lower=c(-1,0.01,0.00001,1,1,0.001,1e-05), effect=c('q'))
+#' data(ex_spmcon1dg)
+#' res <- spm_con_1d_g(ex_data$spm_data, ex_data$gene_data, 
+#' a = -0.02, b=0.2, q=0.01, f=3, f1=3, mu0=0.01, theta=1e-05, 
+#' upper=c(-0.01,3,0.1,10,10,0.1,1e-05), lower=c(-1,0.01,0.00001,1,1,0.001,1e-05), 
+#' effect=c('q'))
 #'}
 spm_con_1d_g <- function(spm_data, gene_data, a = NA, b = NA, q = NA, f = NA, f1 = NA, mu0 = NA, theta = NA, effect = c('a'), lower = c(), upper = c(), control = list(xtol_rel = 1e-6), global = FALSE, verbose = TRUE, ahessian = FALSE)
 {
